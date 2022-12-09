68f76807f089a0b465b1b94619994b3c03a65e41
haccht <haccht@users.noreply.github.com>
2022-12-10 00:38:30 +0900

    Add feature to render the output in realtime

--- scrapli_ipython/scrapli_ipython.py
+++ scrapli_ipython/scrapli_ipython.py
@@ -1,3 +1,5 @@
+import sys
+from io import BytesIO
 from getpass import getpass
 from jinja2 import Template
 from scrapli import Scrapli
@@ -11,11 +13,14 @@ from IPython.core.getipython import get_ipython
 class ScrapliMagics(Magics):
     def __init__(self, shell):
         super(ScrapliMagics, self).__init__(shell)
+        self._timeout = 30
         self._platform = ''
         self._connection = None
 
-    def _connect(self, host, platform, transport, **kwargs):
+    def _connect(self, host, platform, transport, timeout, **kwargs):
+        timeout  = timeout  or self._timeout
         platform = platform or self._platform
+
         if not platform:
             raise Exception(f"No platform specified")
         if transport not in ["ssh2", "telnet"]:
@@ -28,33 +33,43 @@ class ScrapliMagics(Magics):
                 auth_username=input("Username:"),
                 auth_password=getpass("Password:"),
                 auth_strict_key=False,
+                timeout_socket=timeout,
+                timeout_transport=timeout,
+                channel_log=ChannelLogIO(),
                 **kwargs)
         self._connection.open()
 
     @line_magic
     @magic_arguments.magic_arguments()
-    @magic_arguments.argument('-p', '--platform', type=str, default='', nargs='?')
-    @magic_arguments.argument('-t', '--transport', type=str, default='ssh2')
+    @magic_arguments.argument('-t', '--timeout',   type=int, default=30, nargs='?')
+    @magic_arguments.argument('-p', '--platform',  type=str, default='', nargs='?')
+    @magic_arguments.argument('-T', '--transport', type=str, default='ssh2')
     @magic_arguments.argument('host', type=str)
     def scrapli(self, line):
         args = magic_arguments.parse_argstring(self.scrapli, line)
-        self._connect(args.host, args.platform, args.transport)
+        self._connect(args.host, args.platform, args.transport, args.timeout)
 
     @line_magic
     @magic_arguments.magic_arguments()
+    @magic_arguments.argument('-t', '--timeout',  type=int, default=30, nargs='?')
     @magic_arguments.argument('-p', '--platform', type=str, default='', nargs='?')
     @magic_arguments.argument('host', type=str)
     def ssh(self, line):
         args = magic_arguments.parse_argstring(self.ssh, line)
-        self._connect(args.host, args.platform, "ssh2")
+        self._connect(args.host, args.platform, "ssh2", args.timeout)
 
     @line_magic
     @magic_arguments.magic_arguments()
+    @magic_arguments.argument('-t', '--timeout',  type=int, default=30, nargs='?')
     @magic_arguments.argument('-p', '--platform', type=str, default='', nargs='?')
     @magic_arguments.argument('host', type=str)
     def telnet(self, line):
         args = magic_arguments.parse_argstring(self.telnet, line)
-        self._connect(args.host, args.platform, "telnet")
+        self._connect(args.host, args.platform, "telnet", args.timeout)
+
+    @line_magic
+    def timeout(self, line):
+        self._timeout = int(line)
 
     @line_magic
     def platform(self, line):
@@ -76,9 +91,9 @@ class ScrapliMagics(Magics):
     @magic_arguments.magic_arguments()
     @magic_arguments.argument('var', type=str, default='', nargs='?')
     def cmd(self, line, cell):
+        self._connection.get_prompt()
+        resp = self._connection.send_commands(self._format(cell), timeout_ops=0)
         args = magic_arguments.parse_argstring(self.cmd, line)
-        resp = self._connection.send_commands(self._format(cell))
-        print(resp.result_mp())
         if args.var:
             get_ipython().user_ns[args.var] = resp
 
@@ -86,11 +101,16 @@ class ScrapliMagics(Magics):
     @magic_arguments.magic_arguments()
     @magic_arguments.argument('var', type=str, default='', nargs='?')
     def configure(self, line, cell):
+        self._connection.get_prompt()
+        resp = self._connection.send_configs(self._format(cell), timeout_ops=0)
         args = magic_arguments.parse_argstring(self.cmd, line)
-        resp = self._connection.send_configs(self._format(cell))
-        print(resp.result_mp(separator=""))
         if args.var:
             get_ipython().user_ns[args.var] = resp
+
+
+class ChannelLogIO(BytesIO):
+    def write(self, b, **kwargs):
+        sys.stdout.write(b.decode('utf-8', 'ignore'), **kwargs)
 
 
 # monkey-patch scrapli.response.MultiResponse
